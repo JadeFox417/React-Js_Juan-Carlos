be44dc2237eb668cd037ac45e28e8d063b4cceaf
JadeFox417 <tepanuspardosi.jp@gmail.com>
2022-09-06 14:28:06 +0700

    task 5: completed

---
+++ 08_Javascript-Refreshment/praktikum/task5.js
@@ -0,0 +1,5 @@
+let value = [1, 2, 3, 4, 5, 6];
+
+// Create new function called "newValue" to multiply each of element on "value" array by 2
+let newValue = value.map((element) => element * 2);
+console.log(newValue);

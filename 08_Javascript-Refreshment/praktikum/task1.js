dc7f2c185a4772c4fc242a130bc420f9f1a7a530
JadeFox417 <tepanuspardosi.jp@gmail.com>
2022-09-06 19:33:33 +0700

    task 1: resolved

--- 08_Javascript-Refreshment/praktikum/task1.js
+++ 08_Javascript-Refreshment/praktikum/task1.js
@@ -1,8 +1,8 @@
-var a = 5;
+let a = 5;
 let b = "kampus merdeka";
 const nama = "Budi";
 let terdaftar = false;
-let lengkap_arr = [a, b, nama, terdaftar];
+let lengkapArr = [a, b, nama, terdaftar];
 
 function perkenalan() {
   let asal = "indonesia";

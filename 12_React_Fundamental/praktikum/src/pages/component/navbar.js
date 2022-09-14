27fb4b3a4054479b2b1d939a96feed257da3b8aa
JadeFox417 <tepanuspardosi.jp@gmail.com>
2022-09-15 01:40:49 +0700

    task completed

---
+++ 12_React_Fundamental/praktikum/src/pages/component/navbar.js
@@ -0,0 +1,11 @@
+import "../home.css";
+
+const NavBar = () => {
+  return (
+    <div className="title">
+      <h1 className="titleContent">To Do App</h1>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ 12_React_Fundamental/praktikum/src/pages/home.js
@@ -0,0 +1,13 @@
+import Content from "./component/content";
+import NavBar from "./component/navbar";
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <Content />
+    </>
+  );
+};
+
+export default Home;
